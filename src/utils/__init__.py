1692d18135b4e67aeeba80404db697a0344baab7
Apoorva Saxena <40122084+apoorvasaxena03@users.noreply.github.com>
2026-01-01 22:09:35 -0600

    Update requirements and minor notebook and import changes
    Updated requirements.txt to use unpinned package names and added several new dependencies. Updated notebook kernelspecs and Python versions, and cleared execution counts in spacing_stat_ik_v1.ipynb. Improved import formatting in src/utils/__init__.py for better readability.

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,7 @@
 from .custom_logger import CustomLogger
 from .database_manager import DatabricksOdbcConnector
-from .utils import reorder_columns, clean_column_names, read_csv_with_mapper, read_excel_with_mapper, standardize_column_names, compute_bg_rcat, add_producing_months, calculate_cumulative_volumes_by_period
+from .utils import (reorder_columns, clean_column_names, read_csv_with_mapper, read_excel_with_mapper, 
+                    standardize_column_names, compute_bg_rcat, add_producing_months, calculate_cumulative_volumes_by_period)
 
 __all__ = ["CustomLogger", "DatabricksOdbcConnector", "reorder_columns", 
            "clean_column_names", "read_csv_with_mapper","read_excel_with_mapper", 
